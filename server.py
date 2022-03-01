4e4222dc768efab8344b5dc70f48fc619e6403f2
yarkin <nikita.kondratjuks@student.uib.no>
2022-03-01 15:35:44 +0100

    add some base info for server

--- server.py
+++ server.py
@@ -1,2 +1,18 @@
 # This is where the server for the game is going to be
+import socket
+
+# This is the "public ip" that makes it possible to connect to the internet
+SERVER_PUBLIC_IP = "129.177.197.63"
+# Out base bit header for sending and reciving data
+HEADER = 64
+# Chosen port
+PORT = 5555
+# Addrese from where the data will be comming from
+ADDRESE = (SERVER_PUBLIC_IP, PORT)
+
+# TCP protocol that bind with the addresse
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDRESE)
+
+
 

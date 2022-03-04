f58ef17adfdfb03a88d6ee12d4419075ca164ee6
Ivar Fatland <ivar.fatland@lyse.net>
2022-03-04 15:23:12 +0100

    Skriv in server ip som f.eks '192.168.10.127:5050'

--- client.py
+++ client.py
@@ -1,1 +1,7 @@
-# This is the client file that connect to the server
+import socket
+
+IP_AND_PORT = input('Enter IP: ')
+IP, PORT = IP_AND_PORT.split(sep=':')
+PORT = int(PORT)
+
+SERVER_ADDR = (IP, PORT)

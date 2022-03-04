a4ecab1845156fcf32cd388bb3fe9feac3fc9ac8
Ivar Fatland <ivar.fatland@lyse.net>
2022-03-04 15:22:18 +0100

    Forslag for server.listenForConnectinos() funksjon

--- server.py
+++ server.py
@@ -1,2 +1,33 @@
-# This is where the server for the game is going to be
+import socket
+import threading
 
+SERVER = socket.gethostbyname(socket.gethostname())
+PORT = 5050
+ADDR = (SERVER, PORT)
+
+IP = ':'.join(map(str, ADDR))
+
+server: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+
+def game(p1_socket: socket.socket, p1_address, p2_socket: socket.socket, p2_address):
+    pass
+
+def listenForConnections():
+    
+    server.listen()
+    
+    while True:
+        print(f'Connect client to {IP = } to join game of TNT.')
+        
+        p1_socket, p1_address = server.accept()
+        print('Player 1 connected')
+        
+        p2_socket, p2_address = server.accept()
+        print('Player 2 connected')
+        
+        threading.Thread(target = game, args = (p1_socket, p1_address, p2_socket, p2_address)).start()
+
+if __name__ == '__main__':
+    listenForConnections()

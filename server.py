3c56a7041d106ebfa2063fe7b9fbb9bab463089f
yarkin <nikita.kondratjuks@student.uib.no>
2022-03-04 17:00:24 +0100

    this needs to be worked on a little more

--- server.py
+++ server.py
@@ -1,21 +1,15 @@
 # This is where the server for the game is going to be
-import socket
+from socket import socket
 # Module that allows us to run multiple games at the same time on one server.
-import threading
+from threading import Thread
 
-
-SERVER = socket.gethostbyname(socket.gethostname())
-PORT = 5050
-ADDR = (SERVER, PORT)
-
-# Out base bit header for sending and reciving data
-HEADER = 64
+HEADER = 1024
 
 # Server address in string format
 IP = ':'.join(map(str, ADDR))
+sock = socket()
+sock.bind(("localhost", 5555))
 
-server: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
 
 
 def game(p1_socket: socket.socket, p1_address, p2_socket: socket.socket, p2_address):
@@ -31,18 +25,38 @@ def listenForConnections():
     Then the server will wait for two more connections and repeat.
     '''
     
-    server.listen()
+    sock.listen()
     
     while True:
         print(f'Connect client to {IP = } to join game of TNT.')
         
-        p1_socket, p1_address = server.accept()
+        p1_socket, p1_address = sock.accept()
         print('Player 1 connected')
         
-        p2_socket, p2_address = server.accept()
+        p2_socket, p2_address = sock.accept()
         print('Player 2 connected')
         
-        threading.Thread(target = game, args = (p1_socket, p1_address, p2_socket, p2_address)).start()
+        Thread(target = game, args = (p1_socket, p1_address, p2_socket, p2_address)).start()
+
+# this need more work on :)
+def read(conn):
+  while True:
+    data = conn.recv(1024)
+    if data:
+      sentence = data.decode()
+      new_sentence = sentence.upper()
+      conn.send(new_sentence.encode())
+    else:
+      print('closing', conn)
+      conn.close()
+      break
+
+def accept(sock):
+  while True:
+    conn, address = sock.accept()
+    print('accepted', conn, 'from', address)
+    Thread(target=read, args=(conn,)).start()
+
 
 if __name__ == '__main__':
     listenForConnections()

ebdac6c08d7a93c4d87ff8e61da7dc1bd4903048
boomlich <77412620+boomlich@users.noreply.github.com>
2022-03-04 17:56:02 +0100

    Started communication from client to server

--- client.py
+++ client.py
@@ -1,17 +1,25 @@
-import socket
+from socket import socket
 
-IP_AND_PORT = input('Enter IP: ')
-IP, PORT = IP_AND_PORT.split(sep=':')
-PORT = int(PORT)
+# IP_AND_PORT = input('Enter IP: ')
+# IP, PORT = IP_AND_PORT.split(sep=':')
+# PORT = int(PORT)
 
-SERVER_ADDR = (IP, PORT)
+# SERVER_ADDR = (IP, PORT)
 
+HEADER = 1024
 
 sock = socket()
 server_address = ("localhost", 5555)
 sock.connect(server_address)
-sentence = input("Input lowercase sentence: ")
-sock.send(sentence.encode())
-new_sentence = sock.recv(1024).decode()
-print(f"From Server: {new_sentence}")
+
+champion_list = sock.recv(HEADER)
+client_name = sock.recv(HEADER)
+
+# Print champion list and ask to select champ
+print(champion_list)
+message = input(f"{client_name} select champion: ")
+
+# Check with server if selection is valid
+sock.send((message.encode()))
+
 sock.close()

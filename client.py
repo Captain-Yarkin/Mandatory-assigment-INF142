52dc8cc7b65b1f899af043c96cb55bac78b5048c
Thomas M <boomlich@gmail.com>
2022-03-05 02:18:01 +0100

    Removed unused comments

--- client.py
+++ client.py
@@ -35,17 +35,4 @@ def start():
     _recv()
 
 start()
-
-
-
-# champion_list = sock.recv(globals.HEADER)
-# client_name = sock.recv(globals.HEADER)
-
-# # Print champion list and ask to select champ
-# print(champion_list)
-# message = input(f"{client_name} select champion: ")
-
-# # Check with server if selection is valid
-# sock.send((message.encode()))
-
 sock.close()
